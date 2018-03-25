e26bdaed3216f7032b8a5d1eb7efecd23fd36cfc
rmango <morgan-graham@live.com>
2018-03-24 22:22:12 -0700

    printed list of urls to html popup

--- background.js
+++ background.js
@@ -3,7 +3,12 @@ fetch(chrome.extension.getURL('/siteData.json'))
      .then((resp) => resp.json())
      .then(function (jsonData) {
          console.log(jsonData);
-     
+         for(var k = 0; k < jsonData.length; k++) {
+            var siteEl = document.createElement("DIV");
+            siteEl.innerHTML = jsonData[k].url;
+            document.getElementById("list").appendChild(siteEl);
+            console.log("element created");
+        }
     });
 /*console.log(siteData);
 for(var k = 0; k < siteData.length; k++) {
